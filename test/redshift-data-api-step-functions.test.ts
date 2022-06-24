879acfa5b9542c3e252383f91a590b25d4214aea
tmokmss <tomookam@live.jp>
2022-06-24 14:59:35 +0900

    update to cdk v2

--- test/redshift-data-api-step-functions.test.ts
+++ test/redshift-data-api-step-functions.test.ts
@@ -1,9 +1,10 @@
-import { SynthUtils } from "@aws-cdk/assert";
-import * as cdk from "@aws-cdk/core";
+import * as cdk from "aws-cdk-lib";
+import { Template } from 'aws-cdk-lib/assertions';
 import { RedshiftDataApiStepFunctionsStack } from "../lib/redshift-data-api-step-functions-stack";
 
 test("Snapshot test", () => {
-    const app = new cdk.App();
-    const stack = new RedshiftDataApiStepFunctionsStack(app, "MyTestStack");
-    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  const app = new cdk.App();
+  const stack = new RedshiftDataApiStepFunctionsStack(app, "MyTestStack");
+  const template = Template.fromStack(stack);
+  expect(template).toMatchSnapshot();
 });
